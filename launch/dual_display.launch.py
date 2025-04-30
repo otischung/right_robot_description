59fa39b443c87a03e346b05d0f9b1c2190c40e59
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-04-30 10:43:45 +0800

    [FIX] Add dual display

---
+++ launch/dual_display.launch.py
@@ -0,0 +1,102 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+import xacro
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('left_robot_description')
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'left_robot.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    right_share_dir = get_package_share_directory('right_robot_description')
+
+    right_xacro_file = os.path.join(right_share_dir, 'urdf', 'right_robot.xacro')
+    right_robot_description_config = xacro.process_file(right_xacro_file)
+    right_robot_urdf = right_robot_description_config.toxml()
+
+    rviz_config_file = os.path.join(share_dir, 'config', 'dual_display.rviz')
+
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='True'
+    )
+
+    show_gui = LaunchConfiguration('gui')
+
+    left_robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='left_robot_state_publisher',
+        namespace='left',
+        parameters=[
+            {'robot_description': robot_urdf}
+        ]
+    )
+
+    right_robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='right_robot_state_publisher',
+        namespace='right',
+        parameters=[
+            {'robot_description': right_robot_urdf}
+        ]
+    )
+
+    left_joint_state_publisher_node = Node(
+        condition=UnlessCondition(show_gui),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='left_joint_state_publisher',
+        namespace='left'
+    )
+
+    left_joint_state_publisher_gui_node = Node(
+        condition=IfCondition(show_gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='left_joint_state_publisher_gui',
+        namespace='left',
+    )
+
+    right_joint_state_publisher_node = Node(
+        condition=UnlessCondition(show_gui),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='right_joint_state_publisher',
+        namespace='right'
+    )
+
+    right_joint_state_publisher_gui_node = Node(
+        condition=IfCondition(show_gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='right_joint_state_publisher_gui',
+        namespace='right',
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gui_arg,
+        left_robot_state_publisher_node,
+        right_robot_state_publisher_node,
+        left_joint_state_publisher_node,
+        left_joint_state_publisher_gui_node,
+        right_joint_state_publisher_node,
+        right_joint_state_publisher_gui_node,
+        rviz_node
+    ])
